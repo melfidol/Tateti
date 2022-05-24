6b48022399d0ec8a5e964c4a5b9b6e234cc53b66
Delfina Moldes <delfimoldes@gmail.com>
2022-05-24 12:42:28 -0300

    Agregue style y js

---
+++ juego.js
@@ -0,0 +1,6 @@
+let turno = 1;
+let fichas ["O" , "X"];
+let puestas = 0;
+let partidaTerminada = false;
+let textoVictoria = document.getElementById("TextoVictoria");
+let botones = Array.from(document.getElementBytagName("button"));
