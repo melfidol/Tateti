46ac40bcb3d5484d25fcff9383c9a17cb78ba19a
Delfina Moldes <delfimoldes@gmail.com>
2022-05-26 13:03:47 -0300

    cambios js

--- juego.js
+++ juego.js
@@ -8,7 +8,21 @@ let botones = Array.from(document.getElementBytagName("button"));
 botones.forEach(X => x.addEventListener("click", ponerFicha));
 
 function ponerFicha(event){
-    let
+    let botonPulsado = event.target;
+    if (!partidaTerminada && botonPulsado.innerHTML ==""){
+        botonPulsado.innerHTML = fichas[turno];
+        puestas += 1;
+
+        let estadoPartida = estado();
+        if(estadoPartida == 0){
+            cambiarTurno();
+            if(puestas < 9){
+                ia();
+                estadoPartida = estado();
+                puestas += 1;
+                cambiarTurno();
+            }
+        }
+    }
+
 }
-
-
