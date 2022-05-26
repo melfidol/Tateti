8a2b4db41070750ebc889cb47c92bcf9b55bdd33
Delfina Moldes <delfimoldes@gmail.com>
2022-05-26 12:02:34 -0300

    Primeras funciones js

--- juego.js
+++ juego.js
@@ -1,6 +1,14 @@
 let turno = 1;
-let fichas ["O" , "X"];
+let fichas = ["O" , "X"];
 let puestas = 0;
 let partidaTerminada = false;
-let textoVictoria = document.getElementById("TextoVictoria");
+let textoGanador = document.getElementById("TextoGanador");
 let botones = Array.from(document.getElementBytagName("button"));
+
+botones.forEach(X => x.addEventListener("click", ponerFicha));
+
+function ponerFicha(event){
+    let
+}
+
+
