1dd6c7847d40e619648ade87ebe44c6a2c9ef114
Delfina Moldes <delfimoldes@gmail.com>
2022-06-09 12:47:05 -0300

    focus, iconoMax, ninguno funciona xd

--- funciones/index.js
+++ funciones/index.js
@@ -1,7 +1,7 @@
 const { ipcRenderer } = require("electron")
 
 const botonMin = document.getElementById("botonMin")
-const botonMaxRestore = document.getElementById("botonMax")
+const botonMaxRestore = document.getElementById("botonMaxRestore")
 const botonCerrar = document.getElementById("botonCerrar")
 
 //ESCUCHA MINIMIZAR, sendea mensaje para que le main lo ejecute
@@ -25,6 +25,45 @@ ipcRenderer.on("isRestored", () => {
 
 ipcRenderer.on("isMaximized", () => {
     ipcRenderer.send("unmaximize")
+})
+
+//Cambiar icono maximizar
+function changeMaxResBtn(isMaximizedApp) {
+    if (isMaximizedApp) {
+      botonMaxRestore.style.backgroundImage = "url('src/icon_restore.png')"
+    } else {
+        botonMaxRestore.style.backgroundImage = "url('src/icon_maximize.png')"
+    }
+}
+  
+  
+// Escuchamos el proceso de la aplicación cuando nos dice que se maximiza o se restaura.
+ipcRenderer.on("isMaximized", () => {
+changeMaxResBtn(true)
+})
+ipcRenderer.on("isRestored", () => {
+changeMaxResBtn(false)
+})
+
+
+
+//Cambiar focus
+function changeColorFocus(isFocus) {
+if (isFocus) {
+    barra.style.backgroundColor = '#212A5E'
+} else {
+    barra.style.backgroundColor = '#3a3f5d'
+}
+}
+
+
+// Escuchamos el proceso de la aplicación cuando nos dice que se maximiza o se restaura.
+ipcRenderer.on("isFocus", () => {
+changeColorFocus(true)
+})
+
+ipcRenderer.on("isInactive", () => {
+changeColorFocus(false)
 })
 
 
