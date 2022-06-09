a938cd03cd5284e9b903e6cba5f51eb1021280ec
Delfina Moldes <delfimoldes@gmail.com>
2022-06-09 02:30:38 -0300

    funciona boton minimizar y cerrar, maximizar sigue roto

--- funciones/index.js
+++ funciones/index.js
@@ -1,26 +1,40 @@
+const { ipcRenderer } = require("electron")
 
 const botonMin = document.getElementById("botonMin")
-const botonMax = document.getElementById("botonMax")
+const botonMaxRestore = document.getElementById("botonMax")
 const botonCerrar = document.getElementById("botonCerrar")
 
+//ESCUCHA MINIMIZAR, sendea mensaje para que le main lo ejecute
 botonMin.addEventListener("click", () => {
-    alert("minimizado")
+    ipcRenderer.send("minimize")
 })
 
-botonMax.addEventListener("click", () => {
-    alert("maximizar")
+//no anda gg
+botonMaxRestore.addEventListener("click", () => {
+    
+    if(mainWindow.ismaximized()) ipcRenderer.send("unmaximize")
+    else ipcRenderer.send("maximize")
 })
 
+//ESCUCHA CERRAR
 botonCerrar.addEventListener("click", () => {
-    alert("cerrar")
+    ipcRenderer.send("close")
 })
 
-//Función común, 
+
+
+
+
+/*Función común, 
+
 function minimizarClick() {
 	Alert("Click función común")
 }
 
 //Función flecha,
+
 const minimizarClick = () => {
 	Alert("Click función común")
 }
+
+*/
