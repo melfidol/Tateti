b54f52d23ad28c41eca54764a4f425413209c931
Dolfina Carballeira <dolfinacg@gmail.com>
2022-09-01 11:55:18 -0300

    Arreglos

--- react/src/components/Titlebar/index.tsx
+++ react/src/components/Titlebar/index.tsx
@@ -2,13 +2,13 @@ import style from './Titlebar.module.css'
 
 function Titlebar(){
     return(
-        <div id="barra">
-        <button id="botonMenu" className={style.botonBarra} value="M" title='lam'></button>
-        <div id="titulobarra">TTT</div>
-        <div id="botones">
-            <button id="botonMin" className={style.botonBarra} value="-" title='lam'></button>
-            <button id="botonMax" className={style.botonBarra} value="C" title='lam'></button>
-            <button id="botonCerrar" className={style.botonBarra} value="X" title='lam'></button>
+        <div id="barra" className={style.barra}>
+        <button id="botonMenu" className={[style.botonBarra, style.botonMenu].join(" ")} value="M" title='lam'></button>
+        <div id="titulobarra" className={style.titulobarra}>TTT</div>
+        <div id="botones" className={style.botones}>
+            <button id="botonMin" className={[style.botonBarra, style.botonMin].join(" ")} value="-" title='lam'></button>
+            <button id="botonMax" className={[style.botonBarra, style.botonMax].join(" ")} value="C" title='lam'></button>
+            <button id="botonCerrar" className={[style.botonBarra, style.botonCerrar].join(" ")} value="X" title='lam'></button>
         </div>
     </div>
     )
