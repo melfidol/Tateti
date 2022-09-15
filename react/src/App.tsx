ee38b026a7c6d444821b59bbe3fb9d292526fcd0
Julieta Alegre <julialegre10@gmail.com>
2022-09-15 11:49:51 -0300

    j añado e instalo routes

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,5 @@
+import { Login } from '@mui/icons-material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Titlebar from './components/Titlebar';
 import Index from './views/Index/inicio';
@@ -6,6 +8,14 @@ const ipcRenderer = window.require("electron").ipcRenderer
 function App() {
   return (
   <div className='mainApp'> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+
+    
+
   <Titlebar/>
   <Index/>  
   </div>
